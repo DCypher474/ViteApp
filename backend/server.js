402c0e5e44a2ade5e0be230fb74f937027352298
DCypher <dcypher474@gmail.com>
2024-12-06 23:10:54 +0800

    wqqweqwe

--- backend/server.js
+++ backend/server.js
@@ -20,14 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: function(origin, callback) {
-        const allowedOrigins = process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',') : ['http://localhost:5173'];
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
