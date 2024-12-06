39b637e2183c098593325dface45337cbfa47ab1
DCypher <dcypher474@gmail.com>
2024-12-06 22:43:39 +0800

    tailwind uninstall

--- clients/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"], // Include .jsx files
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
