3feee49e11614e280af0449a3db2dbb797c0c10d
DCypher <dcypher474@gmail.com>
2024-12-06 22:22:02 +0800

    first commit

--- clients/vite.config.js
+++ clients/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/vite.svg',
+            src: '/192x192.png',
             sizes: '192x192',
             type: 'image/svg+xml',
             purpose: 'any maskable'
